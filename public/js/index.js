e7e962d56cfac7496b3ae2fc99f1bc246f7217b1
JotaPe <hernolas@gmail.com>
2018-04-17 18:01:09 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Chat User',
-    text: 'Hey, body text example'
-  });
-
 });
 
 socket.on('newMessage', function(message) {
