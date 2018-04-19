abb7b73ad9ad477e8775117cde121d25ac69c9e6
JotaPe <hernolas@gmail.com>
2018-04-19 11:44:01 +0100

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,10 @@ socket.on('connect', function () {
   console.log('Connected to server');
 });
 
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
 socket.on('newMessage', function(message) {
   console.log('newMessage', message);
   var li = jQuery('<li></li>');
@@ -12,8 +16,14 @@ socket.on('newMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
-socket.on('disconnect', function () {
-  console.log('Disconnected from server');
+socket.on('newLocationMessage', function(message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My Current Location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function(oEvent) {
@@ -25,4 +35,21 @@ jQuery('#message-form').on('submit', function(oEvent) {
   }, function() {
 
   });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(oEvent) {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  }, function() {
+    alert('Unable to fetch location.');
+  })
+
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,27 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
-  it('should generate the correct message object', () => {
-    var from = 'FromValue';
+  it('should generate correct message object', () => {
+    var from = 'GenerateMessageFrom';
     var text = 'TextValue';
     var message = generateMessage(from, text);
-    
+
     expect(message).toInclude({from,text});
     expect(typeof(message.createdAt)).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'GenerateLocationFrom';
+    var latitude = '1';
+    var longitude = '0';
+    var url = 'https://www.google.com/maps?q=1,0';
+    var locationMessage = generateLocationMessage(from, latitude, longitude);
+
+    expect(locationMessage).toInclude({from,url});
+    expect(typeof(locationMessage.createdAt)).toBe('number');
   });
 });
